dbb733f46b6adcd510040566575e54bc1c9c6491
emilyxzhou <zhouxemily@gmail.com>
2022-06-10 15:34:31 -0700

    Updates to loading/training scripts
    Signed-off-by: emilyxzhou <zhouxemily@gmail.com>

--- scripts/train.py
+++ scripts/train.py
@@ -1,30 +1,62 @@
+import git
+import imgaug
+import itertools
+import keras_ocr
+import math
 import matplotlib.pyplot as plt
+import numpy as np
 import os
+import random
+import sklearn.model_selection
+import string
+import tensorflow as tf
 
-from os.path import isfile
+from tools import load_training_data
 
 
-def load_training_data():
-    cwd = os.getcwd()
-    train_data_path = os.path.join(
-        cwd, "..", "data", "training"
+# assert tf.config.list_physical_devices('GPU'), 'No GPU is available.'
+
+cwd = os.getcwd()
+git_repo = git.Repo(cwd, search_parent_directories=True)
+git_root = git_repo.git.rev_parse("--show-toplevel")
+train_folder = os.path.join(
+    git_root, "data", "training"
+)
+train_folder = train_folder.replace("/", "\\")
+
+dataset = load_training_data(train_folder, 60)
+train_labels = [(filepath, box, word.lower()) for filepath, box, word in dataset]
+
+recognizer = keras_ocr.recognition.Recognizer()
+recognizer.compile()
+
+batch_size = 8
+augmenter = imgaug.augmenters.Sequential([
+    imgaug.augmenters.GammaContrast(gamma=(0.25, 3.0)),
+])
+
+train_labels, validation_labels = sklearn.model_selection.train_test_split(train_labels, test_size=0.2, random_state=42)
+(training_image_gen, training_steps), (validation_image_gen, validation_steps) = [
+    (
+        keras_ocr.datasets.get_recognizer_image_generator(
+            labels=labels,
+            height=recognizer.model.input_shape[1],
+            width=recognizer.model.input_shape[2],
+            alphabet=recognizer.alphabet,
+            augmenter=augmenter
+        ),
+        len(labels) // batch_size
+    ) for labels, augmenter in [(train_labels, augmenter), (validation_labels, None)]
+]
+training_gen, validation_gen = [
+    recognizer.get_batch_generator(
+        image_generator=image_generator,
+        batch_size=batch_size
     )
-    char_image_paths = [
-        f for f in os.listdir(
-            os.path.join(train_data_path, "characters")
-        )
-    ]
-    symbol_image_paths = [
-        f for f in os.listdir(
-            os.path.join(train_data_path, "symbols")
-        )
-    ]
+    for image_generator in [training_image_gen, validation_image_gen]
+]
 
-    return char_image_paths + symbol_image_paths
-
-
-if __name__ == "__main__":
-    cwd = os.getcwd()
-    train_data_path = os.path.join(
-        cwd, "..", "data", "training"
-    )
+for i in range(50):
+    image, text = next(training_image_gen)
+    print('text:', text)
+    plt.imshow(image)
--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,14 +1,22 @@
 import cv2
 import git
+import numpy as np
 import os
+import random
 
 
-def load_training_data(train_folder):
+def load_training_data(train_folder, num_files):
+    # TODO: add train/test split option
     image_paths = [
         f for f in os.listdir(train_folder)
     ]
+    indices = random.sample(range(0, len(image_paths)), num_files)
+    image_paths = [image_paths[i] for i in indices]
     boxes, labels = _read_labels(train_folder, image_paths)
-    return list(zip(image_paths, boxes, labels))
+    full_image_paths = [
+        os.path.join(train_folder, f) for f in image_paths
+    ]
+    return list(zip(full_image_paths, boxes, labels))
 
 
 def _read_labels(train_folder, file_paths):
@@ -48,12 +56,12 @@ def _get_box(image_path):
     max_row = max(white_rows)
     max_col = max(white_cols)
 
-    return [
-        (min_row, min_col),
-        (min_row, max_col),
-        (max_row, max_col),
-        (max_row, min_col)
-    ]
+    return np.asarray([
+        [min_row, min_col],
+        [min_row, max_col],
+        [max_row, max_col],
+        [max_row, min_col]
+    ])
 
 
 def show_cropped(image_path):
@@ -78,4 +86,3 @@ if __name__ == "__main__":
     train_folder = train_folder.replace("/", "\\")
 
     dataset = load_training_data(train_folder)
-    print(dataset[0])
