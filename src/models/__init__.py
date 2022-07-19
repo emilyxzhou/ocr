dab5f904b7678154a833dc176c29add2c29dad30
emilyxzhou <zhouxemily@gmail.com>
2022-07-19 11:49:51 -0700

    Update model parameters
    Signed-off-by: emilyxzhou <zhouxemily@gmail.com>

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,9 +2,9 @@ from models.cnn import cnn
 from sklearn.neural_network import MLPClassifier
 
 
-def mlp():
+def mlp(hidden_layer_sizes=(150, 100, 50)):
     return MLPClassifier(
-        hidden_layer_sizes=(150, 100, 50),
+        hidden_layer_sizes=hidden_layer_sizes,
         max_iter=300,
         activation="relu",
         solver="adam",
